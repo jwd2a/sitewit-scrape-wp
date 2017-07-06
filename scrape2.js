005da17f342237ea27f01881b7a58b4eb4f7f948
jwd2a <jdavis1002@gmail.com>
2017-07-06 13:39:20 -0400

    Getting names and counts

--- scrape2.js
+++ scrape2.js
@@ -1,11 +1,20 @@
 var Nightmare = require('nightmare');
 var nightmare = Nightmare({show: true});
 
+var getTitle = function(node) {
+  return node.querySelector('.entry-title > a').innerHTML;
+}
+
 nightmare
   .goto('https://wordpress.org/plugins/search/appointment/page/1')
   .wait('.entry-title > a')
   .evaluate(function() {
-    return Array.from(document.querySelectorAll('.entry-title > a')).map(element => element.innerHTML);
+    return Array.from(document.querySelectorAll('.plugin-card')).map(function(node){
+      return {
+        title: node.querySelector('.entry-title > a').innerHTML,
+        count: node.querySelector('.active-installs').innerText.split("+")[0]
+      }
+    });
   })
   .end()
   .then(function(result) {
@@ -14,3 +23,6 @@ nightmare
   .catch(function(error) {
     console.log('Error', error);
   });
+
+
+
