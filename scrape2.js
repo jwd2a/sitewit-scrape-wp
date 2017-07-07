08ea803af74a55d5990d99e3c9910a2e860da8e3
jwd2a <jdavis1002@gmail.com>
2017-07-07 13:35:50 -0400

    Creating the workbook

--- scrape2.js
+++ scrape2.js
@@ -3,6 +3,8 @@ var nightmare = Nightmare({show: false});
 var async = require('async');
 var fs = require('fs');
 var striptags = require('striptags');
+var stats = require('stats-lite');
+var excel = require('exceljs');
 
 var searchTerm = process.argv[2].replace(' ', '+');
 console.log(searchTerm);
@@ -12,6 +14,7 @@ var getTitle = function(node) {
 }
 
 var completedResults = 'Plugin\tDescription\tLink\tInstalls\n';
+var counts = [];
 
 nightmare
   .goto('https://wordpress.org/plugins/search/' + searchTerm +'/page/1')
@@ -49,6 +52,7 @@ nightmare
             /* Ignore super low installs */
             if (item.count && item.count.substring(1,2) != 'F') {
               completedResults += item.title + "\t" + cleanText(item.description) + "\t" + item.link + "\t" + item.count + "\n";
+              counts.push(item.count.replace(',', ''));
             }
           });
           cb(null, results);
@@ -56,6 +60,7 @@ nightmare
     }, function(){
       fs.writeFile(searchTerm.replace("+", "-") + '.txt', completedResults, function() {
         console.log('ALL DONE BUCKO!');
+        buildWorksheet();
       });
     });
   });
@@ -64,4 +69,40 @@ function cleanText(text) {
   text = striptags(text);
   text = text.replace(/[^a-zA-Z0-9_ ]/g, "");
   return text;
+}
+
+function getWorksheet(callback){
+  var workbook = new excel.Workbook();
+  if (!fs.existsSync('plugins.xlsx')) {
+    workbook = new excel.Workbook();
+    callback(workbook);
+  } else {
+    workbook.xlsx.readFile('plugins.xlsx')
+      .then(function() {
+        callback(workbook);
+      });
+  }
+}
+
+function buildWorksheet() {
+  getWorksheet(function(workbook) {
+    var worksheet = workbook.addWorksheet(searchTerm);
+    worksheet.columns = [
+      { header: 'Plugin' },
+      { header: 'Description' },
+      { header: 'Link' },
+      { header: 'Installs' }
+    ];
+    fs.readFile(searchTerm + '.txt', 'utf8', function(err, file) {
+      var items = file.split('\n');
+      items.forEach(function(item) {
+        var i = item.split('\t');
+        worksheet.addRow([i[0], i[1], i[2], i[3]]);
+      });
+      workbook.xlsx.writeFile('./plugins.xlsx')
+        .then(function() {
+          console.log("Done!");
+        });
+    });
+  });
 }
